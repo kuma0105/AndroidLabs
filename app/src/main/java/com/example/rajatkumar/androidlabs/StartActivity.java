700201c5b414169c1d7186360f8184d7df5e3c2d
rajat kumar <sevenstargarg6@gmail.com>
2018-03-09 15:18:37 -0500

    done lab5

--- app/src/main/java/com/example/rajatkumar/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/rajatkumar/androidlabs/StartActivity.java
@@ -13,7 +13,7 @@ public class StartActivity extends Activity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         Log.i(ACTIVITY_NAME, "in onCreate(): ");
